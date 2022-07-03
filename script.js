43a20608c7fbd4710b1010425680e99d4784eab6
aerahrah <61181505+aerahrah@users.noreply.github.com>
2022-07-04 01:03:52 +0800

    Add files via upload
    Opmitized code

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+//Global Variables
 let firstNum;
 let secondNum;
-let thirdNum;
 let CheckAnswer;
 let AnswerText;
-let alterPut1;
-let alterPut2;
 let score = 0;
 let highscore = 0;
 let temp = 0;
-let round = 0;
+let round;
 
-
+let questions = document.querySelector(".questions");
 const question = document.querySelector(".question");
 const choice_1 = document.getElementById("choice-1");
 const choice_2 = document.getElementById("choice-2");
@@ -24,12 +22,7 @@ const choice = document.querySelectorAll(".choice");
 
 
 numberGenerator = () =>{
-    let RandomTwoPicker = [];
-    for (let j = 0; j < 2; j++) {
-        thirdNum = Math.trunc(Math.random() * 2);
-        RandomTwoPicker[j] = thirdNum;
-    }
-
+    let thirdNum = Math.trunc(Math.random() * 2);
     firstNum = Math.trunc(
         (Math.random() * 25 + 2) * (Math.round(Math.random()) ? 1 : -1)
     );
@@ -38,7 +31,7 @@ numberGenerator = () =>{
         (Math.random() * 21 + 8) * (Math.round(Math.random()) ? 1 : -1)
     );
 
-    if (RandomTwoPicker[0] === 0) {
+    if (thirdNum == 0) {
         CheckAnswer = firstNum + secondNum;
         AnswerText = `${firstNum} + ${secondNum}`;
     } else {
@@ -51,7 +44,7 @@ alter_answer = () =>{
     let random;
     let alterAnswer;
 
-    random= Math.trunc(Math.random() * 5);
+    random= Math.trunc(Math.random() * 4);
     console.log(random);
     switch(random){
         case 0:{
@@ -63,7 +56,7 @@ alter_answer = () =>{
             return alterAnswer;
         }
         case 1:{
-            alterAnswer = firstNum + secondNum;
+            alterAnswer =  secondNum + firstNum ;
             if(CheckAnswer==alterAnswer){
                 alterAnswer= alterAnswer+2;
             }
@@ -79,15 +72,7 @@ alter_answer = () =>{
             return alterAnswer;
         }
         case 3:{
-            alterAnswer = (CheckAnswer-1)*-1;
-            if(CheckAnswer==alterAnswer){
-                alterAnswer= alterAnswer+1;
-            }
-            console.log(alterAnswer);
-            return alterAnswer;
-        }
-        case 4:{
-            alterAnswer = firstNum - secondNum-1;
+            alterAnswer = firstNum - (secondNum*-1);
             if(CheckAnswer==alterAnswer){
                 alterAnswer= alterAnswer+2;
             }
@@ -119,92 +104,62 @@ choiceClassAdder = (c1, c2, c3) =>{
     answerHandleEvent(c1,c2,c3);
 
 };
-answerHandleEvent = (c1,c2,c3) =>{
-    let questions = document.querySelector(".questions");
-    let text = c1.innerHTML; 
+handleFunction = (c1,result1,class1, class2) =>{
 
+        questions.classList.remove(class2);
+        questions.classList.add(class1);
+
+        choice.forEach(choice =>{
+            choice.classList.remove("header-correct");
+            choice.classList.remove("header-wrong");
+            choice.disabled = true;
+        })
+        c1.classList.add("header-correct");
+        result.textContent=result1;
+        scoreAdd.textContent = `score: ${temp}`;
+        next.disabled = false;
+};
+answerHandleEvent = (c1,c2,c3) =>{
+    let text = c1.innerHTML; 
     questions.disabled=true;
     c1.addEventListener("click", ()=>{
         if(text == CheckAnswer){
             score++;
             temp = score;
-            console.log("score clicked "+score)
-            questions.classList.remove("header-wrong");
-            questions.classList.add("header-correct");
-            choice.forEach(choice =>{
-                choice.classList.remove("header-correct");
-                choice.classList.remove("header-wrong");
-            })
-            c1.classList.add("header-correct");
             result.textContent="correct";
             scoreAdd.textContent = `score: ${score}`;
-            next.disabled = false;
-            c3.disabled = true;
-            c2.disabled = true;
-            c1.disabled = true;
+            console.log("score clicked "+score);
+            handleFunction(c1,"correct","header-correct","header-wrong");
         }
     });
     c2.addEventListener("click", ()=>{
         if(true){
-            questions.classList.remove("header-correct");
-            questions.classList.add("header-wrong");
-  
-            choice.forEach(choice =>{
-                choice.classList.remove("header-correct");
-                choice.classList.remove("header-wrong");
-            })
+            handleFunction(c1,"wrong","header-wrong","header-correct");
             c2.classList.add("header-wrong");
-            c1.classList.add("header-correct");
-            result.textContent="wrong";
-            scoreAdd.textContent = `score: ${temp}`;
-            next.disabled = false;
-            c2.disabled = true;
-            c3.disabled = true;
-            c1.disabled = true;
         }
     });
     c3.addEventListener("click", ()=>{
         if(true){
-
-            questions.classList.remove("header-correct");
-            questions.classList.add("header-wrong");
-
-
-            choice.forEach(choice =>{
-                choice.classList.remove("header-correct");
-                choice.classList.remove("header-wrong");
-            })
+            handleFunction(c1,"wrong","header-wrong","header-correct");
             c3.classList.add("header-wrong");
-            c1.classList.add("header-correct");
-            result.textContent="wrong";
-            scoreAdd.textContent = `score: ${temp}`;
-            next.disabled = false;
-            c2.disabled = true;
-            c3.disabled = true;
-            c1.disabled = true;
         }
     });
 }
 endGame = () =>{
     if(round == 11){
-        question.textContent = `Game Finish`;
+        question.textContent = `Quiz Finish`;
         result.textContent = `Score: ${score}/${round-1}`;
-        highscore = score;
-        round = 0;
-        score = 0;
-        scoreAdd.textContent = `YEHEY`;
+        scoreAdd.textContent = " ";
         start.textContent ="Start again";
         next.disabled = true;
         start.disabled = false;
         choice.forEach(choice =>{
             choice.disabled = true;
-            choice.textContent = ""
+            choice.textContent = "";
+            choice.style.display="none";
         });
         start.style.display = "inline-block";
         next.style.display= "none";
-        choice.forEach(choice =>{
-            choice.style.display="none";
-        });
     }
 };
 answer_loader = () =>{
@@ -231,11 +186,9 @@ answer_loader = () =>{
         }
     }   
 };
-next.addEventListener("click", ()=>{
-    round++
+gameLoader = () => {
+    round++;
     result.textContent="";
-    let questions = document.querySelector(".questions");
-    start.disabled=true;
     next.disabled=true;
     choice.forEach(choice =>{
         choice.disabled=false;
@@ -244,39 +197,26 @@ next.addEventListener("click", ()=>{
         choice.classList.remove("wrong-answer2");
         choice.classList.remove("header-correct");
         choice.classList.remove("header-wrong");
+        choice.style.display = "block";
     })
     questions.classList.remove("header-correct");
     questions.classList.remove("header-wrong");
     answer_loader();
     endGame();
+};
+next.addEventListener("click", ()=>{
+  gameLoader();
 });
+
 start.addEventListener("click", ()=>{
-    round++
+    round = 0;
     score = 0;
+    start.style.display = "none";
+    start.textContent = "start";
     scoreAdd.style.display= "block";
     scoreAdd.textContent = `score: ${score}`;
-    result.textContent="";
-    start.textContent = "start";
-    start.style.display = "none";
-    let questions = document.querySelector(".questions");
-    next.disabled=true;
-    start.disabled=true;
     next.style.display ="inline-block";
-    choice.forEach(choice =>{
-        choice.style.display="block";
-    });
-    choice.forEach(choice =>{
-        choice.disabled=false;
-        choice.classList.remove("correct-answer");
-        choice.classList.remove("wrong-answer1");
-        choice.classList.remove("wrong-answer2");
-        choice.classList.remove("header-correct");
-        choice.classList.remove("header-wrong");
-    })
-    questions.classList.remove("header-correct");
-    questions.classList.remove("header-wrong");
-    answer_loader();
-    endGame();
+    gameLoader();
 });
 
 
